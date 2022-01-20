a0f952458c5062f2c5a05ef77b2d30959d618bec
lsp12 <jonathankenny852@gmail.com>
2022-01-20 13:47:28 -0500

    validator

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,19 @@ import { UnitMdModule } from './unit-md/unit-md.module';
 import { MaintenanceModule } from './maintenance/maintenance.module';
 import { InventoryModule } from './inventory/inventory.module';
 import { ResponsableModule } from './responsable/responsable.module';
+import { ValidationPipe } from './validation.pipe';
 
 @Module({
-  imports: [CategoryModule, ProviderModule, UsersModule, UnitMdModule, MaintenanceModule, InventoryModule, ResponsableModule],
+  imports: [
+    CategoryModule,
+    ProviderModule,
+    UsersModule,
+    UnitMdModule,
+    MaintenanceModule,
+    InventoryModule,
+    ResponsableModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ValidationPipe],
 })
 export class AppModule {}
